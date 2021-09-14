5ac114f047caf346f8726a1886996ba22de343fb
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-09-14 09:10:45 +0100

    use swagger on prodcution

--- MudBlazorWithJwt.Server/Startup.cs
+++ MudBlazorWithJwt.Server/Startup.cs
@@ -79,7 +79,7 @@ namespace MudBlazorWithJwt.Server
                     Scheme = "Bearer",
                     BearerFormat = "JWT",
                     In = ParameterLocation.Header,
-                    //Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                    
                     Description = " JWT Authorization header using the Bearer scheme. \r\n\r\n Put **_ONLY_** your JWT Bearer token on textbox below!"
                 });
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement
@@ -161,6 +161,19 @@ namespace MudBlazorWithJwt.Server
                 });
 
                 app.UseWebAssemblyDebugging();
+            }
+            //also on production
+            else
+            {
+                app.UseSwagger();
+              
+                app.UseSwaggerUI(c =>
+                {
+
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MudBlazorWithJwt.Server v1");
+
+
+                });
             }
 
             app.UseHttpsRedirection();
